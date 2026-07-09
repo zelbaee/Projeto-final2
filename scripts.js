a4add2d812c329a3981ec1e82ed1e8a8fe8ff5da
zelbaee <zelbae1205@gmail.com>
2026-07-09 03:39:38 -0300

    PROJETO APRESENTÁVEL

---
+++ scripts.js
@@ -0,0 +1,49 @@
+document.getElementById('btnEnviar').addEventListener('click', function () {
+
+    const servicosMarcados = document.querySelectorAll('input[name="servico"]:checked');
+    const servicos = Array.from(servicosMarcados).map(input => input.value);
+
+    if (servicos.length === 0) {
+        alert('Por favor, selecione pelo menos um serviço.');
+        return;
+    }
+
+    const dataHora = document.getElementById('data').value;
+
+    if (!dataHora) {
+        alert('Por favor, selecione a data e o horário.');
+        return;
+    }
+
+    const [dataParte, horaParte] = dataHora.split('T');
+    const [ano, mes, dia] = dataParte.split('-');
+    const dataFormatada = `${dia}/${mes}/${ano}`;
+
+    const servicosTexto = servicos.join(', ');
+
+    const mensagem = `Olá, eu gostaria de agendar: ${servicosTexto}, no dia ${dataFormatada} às ${horaParte}.`;
+
+    const numeroEmpresa = "5543996362474";
+
+    const link = `https://wa.me/${numeroEmpresa}?text=${encodeURIComponent(mensagem)}`;
+
+    window.open(link, '_blank');
+
+    
+});
+
+const elementos = document.querySelectorAll('.reveal');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visivel');
+        } else {
+            entry.target.classList.remove('visivel');
+        }
+    });
+}, {
+    threshold: 0.1
+});
+
+elementos.forEach(el => observer.observe(el));
